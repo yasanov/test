f137b678a691bff92b451f3b24a87151977c5521
yasanov <asanov@hrbox.io>
2024-06-16 14:38:28 +0300

    add params

--- backend/config/params.php
+++ backend/config/params.php
@@ -4,4 +4,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+
+    'teletypeApiBaseUrl' => 'https://api.teletype.app/public/api/v1',
+    'teletypeToken' => 'DU3m-SnW3E_087ybUPHnu-AgUAPhrRkncZLNxHr9a0rEbbxpUnCLmUi7pX9KQ7OQ',
 ];
